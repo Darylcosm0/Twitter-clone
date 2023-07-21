f633ca18517d2c2ea5325ae4ad6532d6562c870e
Darylcosm0 <mutaidaryl@gmail.com>
2023-07-21 12:20:52 +0300

    added tweetboxand styled

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -6,22 +6,19 @@ function TweetBox() {
   return (
     <div className='tweetBox'>
         <form>
-            <div className='TweetBox__input'>
+            <div className='tweetBox__input'>
             <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
             <input
-            onChange={(e) => (e.target.value)}
-            value={''}
+            className='tweetBox__imageInput'
             placeholder="What's happening?"
             type="text"
           />
-
-
             </div>
-            <Button>Tweet</Button>
+            <Button className='.tweetBox__tweetButton'>Tweet</Button>
         </form>
 
     </div>
   )
 }
 
-export default TweetBox
+export default TweetBox;
